e3f03be7d36b3940d891796d930e2bfddb6f9750
rertzer <rertzer@student.42.fr>
2024-10-01 12:05:41 +0200

    kfs1: cross_compiler.sh in progress

---
+++ kfs_1/cross_compiler.sh
@@ -0,0 +1,30 @@
+apt install -y build-essential bison flex libgmp3-dev libmpc-dev libmpfr-dev texinfo libisl-dev
+
+export PREFIX="$PWD/cross"
+export TARGET=i386-elf
+export PATH="$PREFIX/bin:$PATH"
+
+mkdir src
+cd src
+#download binutils gcc in $PWD/src
+
+# install gdb
+wget "http://ftp.gnu.org/gnu/gdb/gdb-7.11.tar.gz"
+tar -xvzf gdb-7.11.tar.gz
+mkdir gdb
+cd gdb
+../gdb-7.11/configure --target=$TARGET --prefix="$PREFIX" --disable-werror
+make all-gdb
+make install-gdb
+
+
+#install binutils
+cd ..
+mkdir build-binutils
+cd build-binutils
+../binutils-?.?.?/configure --target=$TARGET --prefix="$PREFIX" --with-sysroot --disable-nls --disable-werror
+make
+make install
+
+#install gcc
+cd ..

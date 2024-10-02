83b909bb2300d17a0edf7edbe468b005eee68a51
rertzer <rertzer@student.42.fr>
2024-10-02 10:36:06 +0200

    boot.s

--- kfs_1/boot.s
+++ kfs_1/boot.s
@@ -5,8 +5,31 @@ MBFLAGS equ MALIGN | MEMINFO
 MAGIC equ 0x1BADB002
 CHECKSUM equ -(MAGIC + MBFLAGS)
 
+; multiboot header that marks the program as a kernel.
 section .multiboot
 align 4
 	dd MAGIC
 	dd MBFLAGS
 	dd CHECKSUM
+
+; creating a stack
+section .bss
+	align 16
+	stack_bottom:
+		resb 16384
+	stack_top:
+
+; start function: load the stack pointer, call the main kernel
+; and enter in an infinite loop
+section .text
+	global _start:function (_start.end - _start)
+	_start:
+		mov esp, stack_top
+		extern kernel_main
+		call kernel_main
+		cli
+	.hang: hlt
+		jmp .hang
+	.end	jmp .hang
+.end:
+

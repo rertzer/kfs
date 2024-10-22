f1c7da6cbe714de49f1384c35a9f7a04c678b1e2
Whenever <joachim.padovani@gmail.com>
2024-10-22 18:30:14 +0200

    fix: update gdt

--- kfs_1/srcs/gdt.c
+++ kfs_1/srcs/gdt.c
@@ -1,13 +1,16 @@
 #include "kernel.h"
 
+__attribute__((aligned(0x10)))
 gdt_ptr_t gdt_ptr;
-gdt_entry_t gdt[GDT_SIZE];
+
+__attribute__((aligned(0x10)))
+gdt_entry_t* gdt = (gdt_entry_t*)GDT_BUFFER;
 
 static void add_gdt_descriptor(gdt_entry_t* entry, gdt_descriptor_t desc);
 
 void init_gdt() {
-	gdt_ptr.limit = sizeof(gdt) - 1;
-	gdt_ptr.base = (uint32_t)gdt;
+	gdt_ptr.limit = (uint16_t)sizeof(gdt_entry_t) * GDT_SIZE - 1;
+	gdt_ptr.base = (uint32_t)GDT_BUFFER;
 
 	gdt_descriptor_t gdt_descriptor_table[GDT_SIZE] = {GDT_TABLE};
 	for (size_t i = 0; i < GDT_SIZE; ++i) {
@@ -24,5 +27,4 @@ static void add_gdt_descriptor(gdt_entry_t* entry, gdt_descriptor_t desc) {
     entry->access_byte = desc.access;
     entry->limit_high_flags = ((desc.limit >> 16) & 0x0F) | (desc.flags << 4 | 0x0F);
     entry->base_high = (desc.base >> 24) & 0xFF;
-
 }

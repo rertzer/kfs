c2dde834b5b85b680e86bf79aa4dcb993c02c589
Whenever <joachim.padovani@gmail.com>
2024-11-27 16:05:55 +0100

    chore(hexdump) delete hexdump cmd

--- kfs_1/incs/builtin.h
+++ kfs_1/incs/builtin.h
@@ -4,7 +4,6 @@
 #include <stddef.h>
 #include <stdint.h>
 
-typedef enum { ECHO, QWERTY, AZERTY, QUIT, DIVIDE, INT, HELP, TEST, BUILTINS_NB} builtin_t;
 
 #define BUILTINS                                                                \
 	[ECHO] = "echo", [QWERTY] = "qwerty", [AZERTY] = "azerty", [QUIT] = "quit", \

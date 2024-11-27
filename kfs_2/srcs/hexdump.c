51260328eba87359bbc3499b3f9623e1db4fc198
Whenever <joachim.padovani@gmail.com>
2024-11-27 16:12:28 +0100

    fix(hexdump) last edit on 42 test

--- kfs_2/srcs/hexdump.c
+++ kfs_2/srcs/hexdump.c
@@ -13,10 +13,10 @@ void print_line(uint32_t *stack_pointer) {
 }
 
 uint8_t hexdump(void) {
-	char testB[6000] = {};
-	for (size_t i = 0; i < sizeof(testB); i++) {
-		testB[i] = 'B';
-	}
+	// volatile char testB[6000];
+	// for (size_t i = 0; i < 6000; i++) {
+	// 	testB[i] = 'B';
+	// }
 	for (uint32_t *stack_pointer = &stack_top; stack_pointer <= &stack_bottom; stack_pointer++) {		
 		print_line(stack_pointer);
     }
@@ -25,6 +25,8 @@ uint8_t hexdump(void) {
 	printk("Stack top: 0x%08x\n", &stack_bottom);
 
 	printk("Stack size: %d\n", (uint32_t)&stack_bottom - (uint32_t)&stack_top);
+
+
 
 	return (0);
 }

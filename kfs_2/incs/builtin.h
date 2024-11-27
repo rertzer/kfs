293eec03ccdb95f54443df27d0acaa3aaecb6895
Whenever <joachim.padovani@gmail.com>
2024-11-27 16:04:13 +0100

    feat(hexdump) add hexdump cmd

--- kfs_2/incs/builtin.h
+++ kfs_2/incs/builtin.h
@@ -15,21 +15,20 @@ typedef enum {
 	DIVIDE,
 	INT,
 	HELP,
-	TEST,
+	HEXDUMP,
 	BUILTINS_NB
 } builtin_t;
 
 #define BUILTINS                                                                               \
 	[ECHO] = "echo", [QWERTY] = "qwerty", [AZERTY] = "azerty", [KREBOOT] = "kreboot",          \
 	[REBOOT] = "reboot", [HALT] = "halt", [QUIT] = "quit", [DIVIDE] = "divide", [INT] = "int", \
-	[HELP] = "help", [TEST] = "test"
+	[HELP] = "help", [HEXDUMP] = "hexdump"
 
 uint8_t echo(char* word, size_t word_len);
 uint8_t qwerty();
 uint8_t azerty();
 uint8_t kreboot();
 uint8_t reboot();
-uint8_t test();
 uint8_t halt();
 void	freboot();
 

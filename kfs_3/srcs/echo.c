87c3f6eb414c7d4e22b1f8b3fe00730bebdc6d49
Whenever <joachim.padovani@gmail.com>
2024-12-02 11:54:43 +0100

    add kfs3

---
+++ kfs_3/srcs/echo.c
@@ -0,0 +1,12 @@
+#include "builtin.h"
+#include "kernel.h"
+
+extern volatile terminal_t term;
+
+uint8_t echo(char* word, size_t word_len) {
+	for (size_t i = 0; i < word_len; ++i) {
+		term_putchar(word[i]);
+	}
+	term_putchar('\n');
+	return (0);
+}

615561eedd8733e2fbddb41e8b48b0dfa2f75dc6
rertzer <rertzer@student.42.fr>
2025-01-19 16:42:25 +0100

    work in progress: memory_infos builtin

--- kfs_3/incs/builtin.h
+++ kfs_3/incs/builtin.h
@@ -18,19 +18,21 @@ typedef enum {
 	REGISTERS,
 	HEXDUMP,
 	BOOT_INFOS,
+	MEMORY_INFOS,
 	BUILTINS_NB
 } builtin_t;
 
 #define BUILTINS                                                                            \
 	[ECHO] = "echo", [QWERTY] = "qwerty", [AZERTY] = "azerty", [KREBOOT] = "kreboot",       \
 	[REBOOT] = "reboot", [HALT] = "halt", [REGISTERS] = "registers", [HEXDUMP] = "hexdump", \
-	[BOOT_INFOS] = "bootinfos",
+	[BOOT_INFOS] = "bootinfos"[MEMORY_INFOS] = "memoryinfos",
 
 typedef uint8_t (*builtin_fun_t)(char* pointer, size_t len);
 
 #define BUILTIN_FUNCTIONS                                                                        \
 	[ECHO] = echo, [QWERTY] = qwerty, [AZERTY] = azerty, [KREBOOT] = kreboot, [REBOOT] = reboot, \
-	[HALT] = halt, [REGISTERS] = registers, [HEXDUMP] = hexdump, [BOOT_INFOS] = boot_infos,
+	[HALT] = halt, [REGISTERS] = registers, [HEXDUMP] = hexdump, [BOOT_INFOS] = boot_infos,      \
+	[MEMORY_INFOS] = memory_infos
 
 uint8_t	 echo(char* word, size_t word_len);
 uint8_t	 qwerty(char* pointer, size_t len);
@@ -40,6 +42,7 @@ uint8_t	 reboot(char* pointer, size_t len);
 uint8_t	 halt(char* pointer, size_t len);
 uint8_t	 registers(char* pointer, size_t len);
 uint8_t	 boot_infos(char* pointer, size_t len);
+uint8_t	 memory_infos(char* pointer, size_t len);
 void	 freboot();
 uint32_t boom();
 uint32_t get_retaddr();
---
+++ kfs_3/srcs/builtins/memory_infos.c
@@ -0,0 +1,4 @@
+#include "builtin.h"
+#include "memory.h"
+
+uint8_t memory_infos(char* pointer, size_t len);

85cf9abaaa2f814858bfc563a2ecf8b074855e15
rertzer <rertzer@student.42.fr>
2025-02-12 10:44:43 +0100

    refact: term putchar's in a separate file

---
+++ kfs_3/srcs/terminal/putchar.c
@@ -0,0 +1,45 @@
+#include "keycode.h"
+#include "terminal.h"
+
+extern terminal_t term;
+
+size_t term_putstr(const char* str) {
+	size_t len = strlen(str);
+	len = term_putchars(str, len);
+	return (len);
+}
+
+size_t term_putchars(const char* array, size_t len) {
+	for (size_t i = 0; i < len; ++i) {
+		term_putchar(array[i]);
+	}
+	return (len);
+}
+
+size_t term_putchars_ln(const char* array, size_t len) {
+	for (size_t i = 0; i < len; ++i) {
+		term_putchar(array[i]);
+	}
+	term_putchar('\n');
+	return (len);
+}
+
+size_t term_putchar(char c) {
+	switch (c) {
+		case '\t':
+			write_tab();
+			break;
+		case '\n':
+			term_next_line();
+			break;
+		case BACKSPACE:
+			term_backspace();
+			break;
+		default:
+			vga_write(c, term.color, term.column, term.row);
+			term_next();
+			break;
+	}
+
+	return ((size_t)1);
+}
--- kfs_3/srcs/terminal/terminal.c
+++ kfs_3/srcs/terminal/terminal.c
@@ -138,50 +138,6 @@ size_t term_cursor_pos() {
 	return (term.row * VGA_WIDTH + term.column);
 }
 
-!!!!!!!!FICHIER SEPARE POUR LES TERM PUT... !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-	   size_t
-	   term_putstr(const char* str) {
-	size_t len = strlen(str);
-	len = term_putchars(str, len);
-	return (len);
-}
-
-size_t term_putchars(const char* array, size_t len) {
-	for (size_t i = 0; i < len; ++i) {
-		term_putchar(array[i]);
-	}
-	return (len);
-}
-
-size_t term_putchars_ln(const char* array, size_t len) {
-	for (size_t i = 0; i < len; ++i) {
-		term_putchar(array[i]);
-	}
-	term_putchar('\n');
-	return (len);
-}
-
-size_t term_putchar(char c) {
-	switch (c) {
-		case '\t':
-			write_tab();
-			break;
-		case '\n':
-			term_next_line();
-			break;
-		case BACKSPACE:
-			term_backspace();
-			break;
-		default:
-			vga_write(c, term.color, term.column, term.row);
-			term_next();
-			break;
-	}
-
-	return ((size_t)1);
-}
-
 void write_tab() {
 	for (size_t i = 0; i < TAB_SIZE; ++i) {
 		vga_write(' ', term.color, term.column, term.row);

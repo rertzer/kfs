f90d5294760214ee2368645aa8ba52fae4c8e5e0
rertzer <rertzer@student.42.fr>
2025-02-13 14:02:16 +0100

    refact: hexdump

--- kfs_3/srcs/builtins/hexdump.c
+++ kfs_3/srcs/builtins/hexdump.c
@@ -8,39 +8,10 @@ extern volatile uint8_t current_code;
 extern uint32_t			page_dir;
 extern uint32_t			low_kernel_page_table;
 
-static bool sleepy();
-
-void print_line(uint32_t* stack_pointer) {
-	uint32_t* stack_pointer_end = stack_pointer + 6;
-	printk("%08x	", stack_pointer);
-	for (uint32_t* i = stack_pointer; i < stack_pointer_end; i++) {
-		printk("%08x ", *i);
-	}
-	printk("\n");
-}
-static bool sleepy() {
-	keypress_t keypress = init_keypress();
-	bool	   end = false;
-
-	while (true) {
-		keypress.keycode = current_code;
-		current_code = 0;
-		if (keypress.keycode != 0) {
-			keypress = update_keypress(keypress);
-
-			if (keypress.pressed == PRESSED) {
-				if (keypress.ascii == 'q' || keypress.ascii == 'Q') {
-					end = true;
-					break;
-				} else if ((keypress.ascii == 'n') || (keypress.ascii == 'N')) {
-					break;
-				}
-			}
-		}
-		sleep();
-	}
-	return (end);
-}
+static void print_line(uint32_t* line);
+static bool dump_stop(uint8_t* counter);
+static bool sleeping();
+static bool read_keyboard(keypress_t* kp);
 
 void tabledump(void) {
 	// printk("page dir at   0x%08x\n", &page_dir);
@@ -61,20 +32,12 @@ uint8_t hexdump(char* pointer, size_t len) {
 	(void)pointer;
 	(void)len;
 	current_code = 0;
-	// volatile char testB[6000];
-	// for (size_t i = 0; i < 6000; i++) {
-	// 	testB[i] = 'B';
-	// }
-	uint32_t counter = 0;
+	uint8_t counter = 0;
 
-	for (uint32_t* stack_pointer = &stack_top; stack_pointer <= &stack_bottom; stack_pointer++) {
-		print_line(stack_pointer);
-		++counter;
-		if (counter == 15) {
-			counter = 0;
-			if (sleepy()) {
-				break;
-			}
+	for (uint32_t* line = &stack_top; line <= &stack_bottom; ++line) {
+		print_line(line);
+		if (dump_stop(&counter)) {
+			break;
 		}
 	}
 
@@ -83,4 +46,53 @@ uint8_t hexdump(char* pointer, size_t len) {
 	printk("Stack size: %d\n", (uint32_t)&stack_bottom - (uint32_t)&stack_top);
 
 	return (0);
+}
+
+static void print_line(uint32_t* line) {
+	printk("%08x	", line);
+	for (uint32_t i = 0; i < 6; ++i) {
+		printk("%08x ", line[i]);
+	}
+	printk("\n");
+}
+
+static bool dump_stop(uint8_t* counter) {
+	bool quit = false;
+	++*counter;
+
+	if (*counter == 15) {
+		*counter = 0;
+		quit = sleeping();
+	}
+	return (quit);
+}
+
+static bool sleeping() {
+	keypress_t keypress = init_keypress();
+	bool	   end = false;
+	while (true) {
+		if (read_keyboard(&keypress) == true) {
+			if (keypress.ascii == 'q' || keypress.ascii == 'Q') {
+				end = true;
+				break;
+			} else if ((keypress.ascii == 'n') || (keypress.ascii == 'N')) {
+				break;
+			}
+			sleep();
+		}
+	}
+	return (end);
+}
+
+static bool read_keyboard(keypress_t* kp) {
+	bool update = false;
+	kp->keycode = current_code;
+	current_code = 0;
+	if (kp->keycode != 0) {
+		*kp = update_keypress(*kp);
+		if (kp->pressed == PRESSED) {
+			update = true;
+		}
+	}
+	return (update);
 }

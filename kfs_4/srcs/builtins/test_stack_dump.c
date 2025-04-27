97a3c305b32570df043c1ed326013ff3ccd30edd
rertzer <rertzer@student.42.fr>
2025-04-27 13:04:22 +0200

    set as finiched

--- kfs_4/srcs/builtins/test_stack_dump.c
+++ kfs_4/srcs/builtins/test_stack_dump.c
@@ -1,22 +1,21 @@
 #include "builtin.h"
-#include "utils.h"
-#include "panic.h"
 #include "memory.h"
-#include "builtin.h"
+#include "panic.h"
+#include "utils.h"
 
 uint8_t test_stack_dump(size_t argc, char** argv) {
+	volatile char test[2048];
 	if (argc == 2 && strncmp("panic", argv[1], 6) == 0) {
-        volatile char test[2048];
-	    for (int i = 0; i < 2048; ++i) {
-	    	test[i] = 'B';
-	    }
-        dump_stack();
-        hexdump(0, NULL);
-        panic(
-            "Testing stack dump: "
-            "Please run `hexdump dump` after reboot\n");
-        return (0);
-    }
-    readdump(0, NULL);
-    return (0);
+		for (int i = 0; i < 2048; ++i) {
+			test[i] = 'B';
+		}
+		dump_stack();
+		hexdump(0, NULL);
+		panic(
+			"Testing stack dump: "
+			"Please run `readdump` after reboot\n");
+		return (0);
+	}
+	readdump(0, NULL);
+	return (test[0]);
 }

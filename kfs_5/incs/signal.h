ca1a98f6c6d2854ebaccd6c41a08ecd875a9bc6a
rertzer <rertzer@student.42.fr>
2025-10-20 12:04:51 +0200

    fix:signal.h

--- kfs_5/incs/signal.h
+++ kfs_5/incs/signal.h
@@ -20,10 +20,8 @@ typedef enum {
 	SIGTERM,
 	SIGSTKFLT,
 	SIGCHLD,
-
 	SIGCONT,
 	SIGSTOP,
-
 	SIGTSTP,
 	SIGTTIN,
 	SIGTTOU,
@@ -36,18 +34,18 @@ typedef enum {
 	SIGIO,
 	SIGPOLL = 29,
 	SIGPWR,
-	SIGWSYS,
+	SIGSYS,
 	SIGUNUSED = 31
 } signal_t;
 
-#define SIG_DEFAULT_VALUES                                                                                           \
-[SIGHUP] = sig_exit, [SIGINT] = sig_exit, [SIQUIT] = sig_core, [SIGILL] = sig_core, [SIGTRAP] = sig_core, \
-[SIGABRT] = sig_core,  [SIGBUS] = sig_core, [SIGFPE] = sig_core, [SIGKILL] = sig_exit,  [SIGUSR1] = sig_exit, \
-[SIGSEGV] = sig_core, [SIGUSR2] = sig_exit, [SIGPIPE] = sig_exit, [SIGALARM] = sig_exit, [SIGTERM] = sig_exit, \
-[SIGSTKFLT] = sig_exit, [SIGCHLD] = sig_ignore, [SIGCONT = sig_continue, [SIGSTOP] = sig_stop, [SIGTSTP] = sig_stop, \
-[SIGTTIN] = sig_stop, [SIGTTOU] = sig_stop, [SIGURG] = sig_ignore,  [SIGXCPU] = sig_core, [SIGXFSZ] = sig_core, \
-[SIGVTALARM] = sig_exit, [SIGPROF] = sig_exit, [SIGWINCH]= sig_ignore [SIGIO] = sig_exit, [SIGPWR] = sig_exit, \
-[SIGSYS] = sig_core
+#define SIG_DEFAULT_VALUES                                                                                            \
+	[SIGHUP] = sig_exit, [SIGINT] = sig_exit, [SIQUIT] = sig_core, [SIGILL] = sig_core, [SIGTRAP] = sig_core,         \
+	[SIGABRT] = sig_core, [SIGBUS] = sig_core, [SIGFPE] = sig_core, [SIGKILL] = sig_exit, [SIGUSR1] = sig_exit,       \
+	[SIGSEGV] = sig_core, [SIGUSR2] = sig_exit, [SIGPIPE] = sig_exit, [SIGALARM] = sig_exit, [SIGTERM] = sig_exit,    \
+	[SIGSTKFLT] = sig_exit, [SIGCHLD] = sig_ignore, [SIGCONT] = sig_continue, [SIGSTOP] = sig_stop,                   \
+	[SIGTSTP] = sig_stop, [SIGTTIN] = sig_stop, [SIGTTOU] = sig_stop, [SIGURG] = sig_ignore, [SIGXCPU] = sig_core,    \
+	[SIGXFSZ] = sig_core, [SIGVTALARM] = sig_exit, [SIGPROF] = sig_exit, [SIGWINCH] = sig_ignore, [SIGIO] = sig_exit, \
+	[SIGPWR] = sig_exit, [SIGSYS] = sig_core,
 
 typedef void (*sig_handler_t)(void);
 

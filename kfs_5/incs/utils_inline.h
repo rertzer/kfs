112f4ebf7eba90465dd3cb2fc4053134ca959a13
rertzer <rertzer@student.42.fr>
2025-10-25 09:38:14 +0200

    feat: added utils inline functions set_bit and unset_bit

---
+++ kfs_5/incs/utils_inline.h
@@ -0,0 +1,14 @@
+#ifndef UTILS_INLINE_H
+#define UTILS_INLINE_H
+
+#include <stdint.h>
+
+inline uint32_t set_bit(uint32_t field, uint32_t index) {
+	return (field | (uint32_t)1 << index);
+}
+
+inline uint32_t unset_bit(uint32_t field, uint32_t index) {
+	return (field & ~((uint32_t)1 << index));
+}
+
+#endif
---
+++ kfs_5/srcs/kernel/utils_inline.c
@@ -0,0 +1,4 @@
+#include "utils_inline.h"
+
+extern inline uint32_t set_bit(uint32_t field, uint32_t index);
+extern inline uint32_t unset_bit(uint32_t field, uint32_t index);
